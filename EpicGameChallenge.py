4a1d6947d1081e96c3fa2e90205e82233766bf2f
Vasav Shah <vasav.shah97@gmail.com>
2015-12-09 18:25:35 -0500

    First Commit
    This is the program so far, it has the basic board made, with movements
    done, Required is still pawn promotion and en passant

---
+++ EpicGameChallenge.py
@@ -0,0 +1,132 @@
+#Chess Game
+from pygame import *
+from Piece import *
+def checkIfUserWantsToMovePiece(win,h,mx,my,mb):
+   
+    x=mx/(h/8)
+    y=my/(h/8)
+    
+    if (board[y][x].getValue()!="blank" and mb[0]==1) :
+        board[y][x].drawMovesPossible(board,win,h)
+        draw.rect(win,(0,200,50),Rect(board[y][x].getX()*(h/8),board[y][x].getY()*(h/8),h/8,h/8),10)
+       
+    return (x,y)
+def clickedAtSamePos(Px,Py,Cx,Cy,h):
+    return (Px,Py)==(Cx/(h/8),Cy/(h/8))
+
+   
+def drawBoard(win,h):
+    '''
+    Draws the board
+    '''
+    for i in range(8):
+        for j in range(8):
+            if (j%2==0 and i%2==0) or  (j%2==1 and i%2==1):
+                draw.rect(win,(240,240,240),Rect(i*(h/8),j*(h/8),h/8,h/8),0)
+            elif (j%2==1 and i%2==0) or(j%2==0 and i%2==1):
+                draw.rect(win,(0,200,50),Rect(i*(h/8),j*(h/8),h/8,h/8),0)
+def putPieces(win,lst,h):
+    ''' Temporary draws the pieces according to the board
+    '''
+    colours= {"w1":(0,0,155),"w2":(137,123,175),"w3":(99,81,146),"w4":(67,47,117),"w5":(40,22,87),"w6":(21,6,58),"b1":(155,0,0),"b2":(212,106,106),"b3":(170,57,57),"b4":(128,21,21),"b5":(85,0,0),"b6":(117,6,58)}
+    for y in range(8):
+        for x in range(8):
+            if lst[y][x].getOValue()!="0":
+                draw.circle(win,colours[lst[y][x].getOValue()],(lst[y][x].getX()*(h/8)+h/16,lst[y][x].getY()*(h/8)+h/16),h/16)
+    
+def readFile(name):
+    f= open(name,"r")
+    lines=f.read().strip().split("\n")
+    values=[]
+    x=0
+    y=0
+    for l in lines:
+        pces=l.split(" ")
+        v=[]
+        x=0
+        for p in pces:
+            v.append(Piece(p,x,y,False,False,False))
+            x+=1
+        values.append(v)
+        y+=1
+    f.close()
+    return values
+def writeFile(board,name):
+    f=open(name)
+            
+running=True
+screen= display.set_mode((600,600))
+board= readFile("arrangement.txt")
+h=600
+
+state="thinking"
+for y in range(8):
+    for x in range(8):
+        board[y][x].setDangered(board,True)
+while running:
+    for e in event.get():
+        if e.type==QUIT:
+            running=False
+        
+        if e.type==MOUSEBUTTONDOWN:
+            if state=="thinking":
+                state="picking"
+            elif state=="picking":
+                if clickedAtSamePos(x1,y1,mx,my,h):
+                    state="thinking"
+                elif(mx/(h/8),my/(h/8)) in board[y1][x1].showPossibleMoves(board):
+                    board[y1][x1].setMoved(True)
+                    for y in range(8):
+                        for x in range(8):
+                            board[y][x].setDangered(board,False)
+                    if board[y1][x1].getType()=="king" and mx/(h/8)-x1==2 and board[y1][x1].getColour()=="white":
+                           board[y1][x1].setCord(mx/(h/8),my/(h/8))
+                           board[my/(h/8)][mx/(h/8)]=board[y1][x1]
+                           board[y1][x1]=Piece("0",x1,y1,False,False,False)                       
+                           board[7][7].setCord(5,7)
+                           board[7][5]=board[7][7]
+                           board[7][7]=Piece("0",7,7,False,False,False)
+                    elif board[y1][x1].getType()=="king" and x1-mx/(h/8)==2 and board[y1][x1].getColour()=="white":
+                           board[y1][x1].setCord(mx/(h/8),my/(h/8))
+                           board[my/(h/8)][mx/(h/8)]=board[y1][x1]
+                           board[y1][x1]=Piece("0",x1,y1,False,False,False)                       
+                           board[7][0].setCord(3,7)
+                           board[7][3]=board[7][0]
+                           board[7][0]=Piece("0",0,7,False,False,False)
+                    elif board[y1][x1].getType()=="king" and x1-mx/(h/8)==2 and board[y1][x1].getColour()=="black":
+                           board[y1][x1].setCord(mx/(h/8),my/(h/8))
+                           board[my/(h/8)][mx/(h/8)]=board[y1][x1]
+                           board[y1][x1]=Piece("0",x1,y1,False,False,False)                       
+                           board[0][0].setCord(3,0)
+                           board[0][3]=board[0][0]
+                           board[0][0]=Piece("0",0,0,False,False,False)
+                    elif board[y1][x1].getType()=="king" and mx/(h/8)-x1==2 and board[y1][x1].getColour()=="black":
+                           board[y1][x1].setCord(mx/(h/8),my/(h/8))
+                           board[my/(h/8)][mx/(h/8)]=board[y1][x1]
+                           board[y1][x1]=Piece("0",x1,y1,False,False,False)
+                           print"yo"
+                           board[0][7].setCord(5,0)
+                           board[0][5]=board[0][7]
+                           board[0][7]=Piece("0",0,7,False,False,False)
+                    else:
+                        board[y1][x1].setCord(mx/(h/8),my/(h/8))
+                        board[my/(h/8)][mx/(h/8)]=board[y1][x1]
+                        board[y1][x1]=Piece("0",x1,y1,False,False,False)
+                    for y in range(8):
+                        for x in range(8):
+                            board[y][x].setDangered(board,True)
+                    state="thinking"
+                else:
+                    state="picking"
+        
+    drawBoard (screen,600)
+    putPieces(screen,board,600)
+    mx,my=mouse.get_pos()
+    mb=mouse.get_pressed()
+    if state=="thinking":
+        x1,y1= checkIfUserWantsToMovePiece(screen,h,mx,my,mb)
+    elif state=="picking":
+        board[y1][x1].drawMovesPossible(board,screen,h)
+        draw.rect(screen,(0,200,50),Rect(board[y1][x1].getX()*(h/8),board[y1][x1].getY()*(h/8),h/8,h/8),10)
+    display.flip()
+quit()
---
+++ Piece.py
@@ -0,0 +1,182 @@
+from pygame import *
+class Piece:
+    '''
+        This is used to keep track of each pieces values and moves it can do
+    '''
+    def __init__(self,number,x,y,d1,d2,moved):
+        self.originalvalue=number
+        if number[0]=="w":
+            self.colour ="white"
+            pvalue={"1":"pawn","2":"rook","3":"horse","4":"bishop","5":"queen","6":"king"}
+            self.value=pvalue[number[1]]
+        elif number[0]=="b":
+            self.colour="black"
+            pvalue={"1":"pawn","2":"rook","3":"horse","4":"bishop","5":"queen","6":"king"}
+            self.value=pvalue[number[1]]
+        else:
+            self.colour="blank"
+            self.value=""
+        self.x=x
+        self.y=y
+        self.dangerb=d1
+        self.dangerw=d2
+        self.moved=moved
+    def getMovesPossible(self):
+        '''
+        This is used to determine what moves are availabe to the certain piece. 
+        '''
+        horizontalrow=[]
+        horizontalrow1=[]
+        verticalrow=[]
+        verticalrow1=[]
+        rdiagonal=[]
+        rdiagonal1=[]
+        ldiagonal=[]
+        ldiagonal1=[]
+        for i in range(-1,-9,-1):
+            horizontalrow1.append((i,0))
+            verticalrow1.append((0,i))
+            rdiagonal1.append((i,i))
+            ldiagonal1.append((-i,i))
+            
+        for i in range (1,9):
+            horizontalrow.append((i,0))
+            verticalrow.append((0,i))
+            rdiagonal.append((i,i))
+            ldiagonal.append((-i,i))
+        movespossible={"pawn":[[(0,1)]],"rook":[horizontalrow,horizontalrow1,verticalrow,verticalrow1],"bishop":[rdiagonal,rdiagonal1,ldiagonal,ldiagonal1],
+                       "queen":[horizontalrow,horizontalrow1,verticalrow,verticalrow1,rdiagonal,rdiagonal1,ldiagonal,ldiagonal1],
+                            "king":[[(0,1)],[(0,-1)],[(1,0)],[(-1,0)],[(-1,-1)],[(-1,1)],[(1,1)],[(1,-1)]],
+                            "horse":[[(-1,2)],[(-2,1)],[(1,2)],[(2,1)],[(2,-1)],[(1,-2)],[(-1,-2)],[(-2,-1)]],
+                            "":[[]]}
+        if self.value!="pawn":
+            return movespossible[self.value]
+        else:
+            if self.colour=="black":
+                if self.y==1:
+                    return [[(0,1),(0,2)]]
+                else:
+                    return [[(0,1)]]
+            if self.colour=="white":
+                if self.y==6:
+                    return [[(0,-1),(0,-2)]]
+                else:
+                    return [[(0,-1)]]        
+
+        
+    def showPossibleMoves(self,board):
+        '''
+        This checks if the move available are actual possible moves and ensures there is nothing in the way and stuff
+        '''
+        moves=self.getMovesPossible()
+        actualpossiblemoves=[]
+        if self.value=="pawn":
+            if self.colour=="black":
+                nx=self.x+1
+                ny=self.y+1
+                if nx>=0 and nx<8 and ny>=0 and ny<8  and board[ny][nx].getColour()=="white":
+                    moves.append([(1,1)])
+                nx=self.x-1
+                ny=self.y+1
+                if nx>=0 and nx<8 and ny>=0 and ny<8  and board[ny][nx].getColour()=="white":
+                    moves.append([(-1,1)])
+            if self.colour=="white":
+                nx=self.x+1
+                ny=self.y-1
+                if nx>=0 and nx<8 and ny>=0 and ny<8  and board[ny][nx].getColour()=="black":
+                    moves.append([(1,-1)])
+                nx=self.x-1
+                ny=self.y-1
+                if nx>=0 and nx<8 and ny>=0 and ny<8  and board[ny][nx].getColour()=="black":
+                    moves.append([(-1,-1)])
+        if self.value=="king":
+            if self.colour=="white" and self.getMoved()==False and board[7][5].getDanger()[0]==False and board[7][6].getDanger()[0]==False and board[7][7].getMoved()==False:
+                moves[2].append((2,0))
+            if self.colour=="white" and self.getMoved()==False and board[7][4].getDanger()[0]==False and board[7][3].getDanger()[0]==False and board[7][2].getDanger()[0]==False and board[0][7].getMoved()==False:
+                moves[3].append((-2,0))
+            if self.colour=="black" and self.getMoved()==False and board[0][5].getDanger()[1]==False and board[0][6].getDanger()[1]==False and board[0][7].getMoved()==False:
+                moves[3].append((-2,0))
+            if self.colour=="black" and self.getMoved()==False and board[0][4].getDanger()[1]==False and board[0][3].getDanger()[1]==False and board[0][2].getDanger()[1]==False and board[0][7].getMoved()==False:
+                moves[2].append((2,0))
+##        print moves[-1]
+            
+        for row in moves:
+            for i in row:
+                x1=i[0]+self.x
+                y1=i[1]+self.y
+                if x1>=0 and x1<8 and y1>=0 and y1<8:
+                    pieceatpos=board[y1][x1]
+                    if pieceatpos.getColour()!=self.colour:
+                        if pieceatpos.getColour()=="blank":
+                            actualpossiblemoves.append((x1,y1))
+                        else:
+                            if self.value=="pawn" and i[0]==0:
+                                break
+                            actualpossiblemoves.append((x1,y1))
+                            break                   
+                    else:
+                        break
+        return actualpossiblemoves
+    
+    def setDangered(self,board,tf):
+        moves=self.showPossibleMoves(board)
+        if self.value=="pawn":
+            moves=[]
+            if self.colour=="black":
+                nx=self.x+1
+                ny=self.y+1
+                if nx>=0 and nx<8 and ny>=0 and ny<8:
+                    moves.append((self.x+1,self.y+1))
+                nx=self.x-1
+                ny=self.y+1
+                if nx>=0 and nx<8 and ny>=0 and ny<8:
+                    moves.append((self.x-1,self.y+1))
+            if self.colour=="white":
+                nx=self.x+1
+                ny=self.y-1
+                if nx>=0 and nx<8 and ny>=0 and ny<8:
+                    moves.append((self.x+1,self.y-1))
+                nx=self.x-1
+                ny=self.y-1
+                if nx>=0 and nx<8 and ny>=0 and ny<8:
+                    moves.append((self.x-1,self.y-1))
+        for (x,y) in moves:
+            board[y][x].setDanger(tf,self.x,self.y,board)
+
+    def drawMovesPossible(self,board,win,h):
+        '''
+        This just draws the moves possible
+        '''
+        possiblemoves=self.showPossibleMoves(board)
+        for i in possiblemoves:
+            draw.rect(win,(255,0,0),Rect((i[0])*h/8,(i[1])*(h/8),h/8,h/8),4)
+#########################################################################################
+
+    def getX(self):
+        return self.x
+    def getY(self):
+        return self.y
+    def getValue(self):
+        return self.colour+" "+self.value
+    def getColour(self):
+        return self.colour
+    def getOValue(self):
+        return self.originalvalue
+    def getDanger(self):
+        return (self.dangerb,self.dangerw)
+    def getMoved(self):
+        return self.moved
+    def getType(self):
+        return self.value
+    def setCord(self,x,y):
+        self.x=x
+        self.y=y
+    def setDanger(self,tf,x,y,board):
+        if board[y][x].colour=="black" :
+            self.dangerb=tf
+        elif board[y][x].colour=="white":
+            self.dangerw=tf
+    def setMoved(self,x):
+        self.moved=x
+        
+##########################################################################################
