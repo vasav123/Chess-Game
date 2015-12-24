95f692f011be26171506c80329e3cffbf41191c1
Vasav Shah <vasav.shah97@gmail.com>
2015-12-24 03:24:51 -0500

    Update
    Update

--- AiEngine.py
+++ AiEngine.py
@@ -1,42 +1,65 @@
 class AiEngine:
     def __init__(self):
         ""
-    def searchTree(self,node,depth,m):
+    def searchTree(self,node,depth,a,b,m):
         if depth==0 or node.getTerminal():
             return node.getValue()
         if m:
             bestValue=-10000
+            for child in node.getChildren():
+                bestValue=max(bestValue,self.searchTree(child,depth-1,a,b,False))
+                a=max(a,bestValue)
+                if b<=a:
+                    break
+            node.setValue(bestValue)
+            return bestValue
         else:
             bestValue=10000
-        for child in node.getChildren():
-            val=self.searchTree(child,depth-1, not m)
-            if m:
-                bestValue=max(bestValue, val)
-            else:
-                bestValue=min(bestValue,val)
-        node.setValue(bestValue)
-        return bestValue
+            for child in node.getChildren():
+                bestValue=min(bestValue,self.searchTree(child,depth-1,a,b,True))
+                bb=min(b,bestValue)
+                if b<=a:
+                    break
+            node.setValue(bestValue)
+            return bestValue
 
 class Node:
-    def __init__(self,value):
-        self.value=value
+    def __init__(self,board):
+        self.board=board
         self.children=[]
         
+    def evalBoard(self):
+        wvalue=0
+        bvalue=0
+        for y in range(8):
+            for x in range(8):
+                if board[y][x].getColour=="white":
+                    wvalue+=board[y][x].getValue()
+                else:
+                    bvalue+=board[y][x].getValue()
+        return bvalue-wvalue
+    
     def getTerminal(self):
         if len(self.children)==0:
             return True
         else:
             return False
-    def setValue(self,value):
-        self.value=value
-    def getValue(self):
-        return self.value
 
+    def getBoard(self):
+        return self.board
+    
     def getChildren(self):
         return self.children
-
+    
     def addChild(self,node):
         self.children.append(node)
+    
+    def makeAMove(self,move):
+            
+
+def makeTree():
+    n=Node(board,)
+    
 initialnode=Node(0)
 n1=Node(0)
 n2=Node(0)
@@ -51,5 +74,5 @@ n1.addChild(n4)
 n2.addChild(n5)
 n2.addChild(n6)
 x=AiEngine()
-print x.searchTree(initialnode,2,True)
+print x.searchTree(initialnode,2,10000,-10000,True)
 print initialnode.getValue()
